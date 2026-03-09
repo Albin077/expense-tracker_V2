e1c0a490424072ead5b57a3a5840092bd4d008ef
Albin K V <albinkv077@gmail.com>
2026-03-09 23:42:30 +0530

    updated db.py

--- EXP_backend/app/db.py
+++ EXP_backend/app/db.py
@@ -1,7 +1,9 @@
 import os
 import psycopg2
+from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 
 DATABASE_URL = os.getenv("SUPABASE_DB_URL")
@@ -10,9 +12,12 @@ if not DATABASE_URL:
     raise RuntimeError("SUPABASE_DB_URL missing")
 
 def get_db():
-    conn = psycopg2.connect(DATABASE_URL)
     try:
+        conn = psycopg2.connect(
+            DATABASE_URL,
+            sslmode="require",   # required for Supabase external connections
+            cursor_factory=RealDictCursor  # return rows as dictionaries
+        )
         yield conn
     finally:
         conn.close()
-
