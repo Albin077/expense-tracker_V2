5f70b6e6efc047ad3075b52bb5b8ba591648f70a
Albin K V <albinkv077@gmail.com>
2026-03-12 01:35:52 +0530

    ...

--- EXP_backend/app/main.py
+++ EXP_backend/app/main.py
@@ -22,7 +22,7 @@ async def startup():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_origin_regex=r"https://expense-tracker-v2-we38-beowrvfzx-albinkv03-3198s-projects.vercel.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
