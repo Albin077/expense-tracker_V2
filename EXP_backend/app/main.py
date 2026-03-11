9df68a28b986e8c8bb538014a4cd22c496a956ea
Albin K V <albinkv077@gmail.com>
2026-03-12 01:57:45 +0530

    added preflighthandler

--- EXP_backend/app/main.py
+++ EXP_backend/app/main.py
@@ -28,6 +28,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+@app.options("/{rest_of_path:path}")
+async def preflight_handler(rest_of_path: str):
+    return {"message": "OK"}
+
 # ------------------------------
 # HEALTH CHECK
 # ------------------------------
